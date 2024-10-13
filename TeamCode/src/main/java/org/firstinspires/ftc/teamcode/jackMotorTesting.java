3a9c1dacf44380a39b1c9ee941458122af114ad1
allly <allysmith64000@gmail.com>
2024-10-13 15:48:58 -0600

    added motor stop and reduced power from 0.1 to 0.01

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jackMotorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jackMotorTesting.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.yise.ledLights;
 
@@ -12,6 +13,7 @@ public class jackMotorTesting extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
+    public Servo servo1;
 
     private DcMotor left = null;
     private DcMotor right = null;
@@ -23,7 +25,7 @@ public class jackMotorTesting extends LinearOpMode {
         // to the names assigned during the robot configuration step on the DS or RC devices.
         left = hardwareMap.get(DcMotor.class, "left");
         right = hardwareMap.get(DcMotor.class, "right");
-
+        servo1 = hardwareMap.get(Servo.class, "servo1");
 
         left.setDirection(DcMotor.Direction.FORWARD);
         right.setDirection(DcMotor.Direction.REVERSE);
@@ -35,27 +37,34 @@ public class jackMotorTesting extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            //left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+           // right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+           // left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+           // right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+           // left.setTargetPosition(100);
+           //4 right.setTargetPosition(100);
+
+            telemetry.addData("Target position", "left position: " + left.getTargetPosition());
+            telemetry.addData("Target position", "right position: " + right.getTargetPosition());
+            telemetry.addData("Servo position", "servo1 position: " + servo1.getPosition());
 
             if (gamepad1.dpad_down) {
-                left.setPower(0.1f);
-                right.setPower(0.1f);
-                telemetry.addData("Seting power", "1.0");
-                telemetry.update();
+                left.setPower(0.3);
+                right.setPower(0.3);
             } else if (gamepad1.dpad_up) {
-                right.setPower(-0.1f);
-                left.setPower(-0.1f);
-                telemetry.addData("Seting power", "-1.0");
-                telemetry.update();
-            }else {
-                right.setPower(0f);
-                left.setPower(0f);
-                telemetry.addData("Seting power", "0");
-                telemetry.update();
+                right.setPower(-0.3);
+                left.setPower(-0.3);
+            } else if (gamepad1.dpad_left) {
+                servo1.setPosition(0.2);
+            } else if (gamepad1.dpad_right) {
+                servo1.setPosition(0.7);
+            } else {
+                right.setPower(0.0);
+                left.setPower(0.0);
             }
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Current position", "left position: " + left.getCurrentPosition());
+            telemetry.addData("Current position", "right position: " + right.getCurrentPosition());
             telemetry.update();
         }
     }

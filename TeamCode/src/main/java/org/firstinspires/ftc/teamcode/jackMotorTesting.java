0d0c9f29fea01ec13054b86af2893cc14f4222e3
allly <allysmith64000@gmail.com>
2024-10-27 14:27:27 -0600

    Created new lift arm class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jackMotorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jackMotorTesting.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.yise.ledLights;
+import org.firstinspires.ftc.teamcode.yise.liftArm;
 
 @TeleOp(name="jack Motor Testing", group="Linear Opmode")
 public class jackMotorTesting extends LinearOpMode {
@@ -15,20 +15,13 @@ public class jackMotorTesting extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     public Servo servo1;
 
-    private DcMotor left = null;
-    private DcMotor right = null;
-
     @Override
     public void runOpMode() {
+        liftArm arm = new liftArm(hardwareMap);
 
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
-        left = hardwareMap.get(DcMotor.class, "left");
-        right = hardwareMap.get(DcMotor.class, "right");
         servo1 = hardwareMap.get(Servo.class, "servo1");
-
-        left.setDirection(DcMotor.Direction.FORWARD);
-        right.setDirection(DcMotor.Direction.REVERSE);
 
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
@@ -38,26 +31,13 @@ public class jackMotorTesting extends LinearOpMode {
         runtime.reset();
 
         while (opModeIsActive()) {
-            left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-
-            telemetry.addData("Target position", "left position: " + left.getTargetPosition());
-            telemetry.addData("Target position", "right position: " + right.getTargetPosition());
             telemetry.addData("Servo position", "servo1 position: " + servo1.getPosition());
 
             if (gamepad1.dpad_down) {
-                left.setTargetPosition(100);
-                right.setTargetPosition(100);
-                left.setPower(0.3);
-                right.setPower(0.3);
+               arm.setArmPosition(liftArm.armPosition.DOWN);
             } else if (gamepad1.dpad_up) {
-                left.setTargetPosition(0);
-                right.setTargetPosition(0);
-                right.setPower(0.3);
-                left.setPower(0.3);
+                arm.setArmPosition(liftArm.armPosition.UP);
             } else if (gamepad1.dpad_left) {
                 servo1.setPosition(0.2);
             } else if (gamepad1.dpad_right) {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/yise/liftArm.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.yise;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.archived23_24SeaonCenterStage.yiseArchived.LiftArm;
+
+
+public class liftArm {
+    public DcMotor armLeft, armRight;
+    public enum armPosition {
+        DOWN,
+        UP
+    }
+    public armPosition currentArmPosition;
+
+    //Constructor
+    public liftArm(HardwareMap hardwareMap) {
+        //Initialize motors and servos
+        armLeft = hardwareMap.get(DcMotor.class, "armleft");
+        armRight = hardwareMap.get(DcMotor.class, "armright");
+
+        //Set motor directions
+        armRight.setDirection(DcMotor.Direction.FORWARD);
+        armLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        //Reset encoders
+        armLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        setArmPosition(liftArm.armPosition.DOWN);
+    }
+
+    public void setArmPosition(liftArm.armPosition targetArmPosition) {
+        switch (targetArmPosition) {
+            case DOWN:
+                armLeft.setTargetPosition(0);
+                armRight.setTargetPosition(0);
+                armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armRight.setPower(0.25);
+                armLeft.setPower(0.25);
+                break;
+            case UP:
+                armLeft.setTargetPosition(1);
+                armRight.setTargetPosition(-1);
+                armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armLeft.setPower(0.25);
+                armRight.setPower(0.25);
+                break;
+        }
+        currentArmPosition = targetArmPosition;
+    }
+
+
+
+}

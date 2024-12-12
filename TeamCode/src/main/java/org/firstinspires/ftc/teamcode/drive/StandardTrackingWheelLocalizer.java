80022fe47a3efba59faf3dd7401cb22e7f351b8e
JacobWingate <121843757+JacobWingate@users.noreply.github.com>
2024-12-11 17:16:14 -0700

    Auto Pathing and structuring

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -55,16 +55,16 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
         lastEncVels = lastTrackingEncVels;
 
         // motor port 2
-        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "RightBackDrive"));
+        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "RightBackDrive"));
         // motor port 1
-        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "LeftBackDrive"));
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "LeftBackDrive"));
         // motor port 0
         frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "LeftFrontDrive"));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
-        frontEncoder.setDirection(Encoder.Direction.REVERSE);
-        leftEncoder.setDirection(Encoder.Direction.REVERSE);
-        rightEncoder.setDirection(Encoder.Direction.REVERSE);
+        //frontEncoder.setDirection(Encoder.Direction.REVERSE);
+        //leftEncoder.setDirection(Encoder.Direction.REVERSE);
+        //rightEncoder.setDirection(Encoder.Direction.REVERSE);
     }
 
     public static double encoderTicksToInches(double ticks) {

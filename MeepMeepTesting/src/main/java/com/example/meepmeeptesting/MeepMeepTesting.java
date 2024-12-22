44ecd2dccb159440d9256604640e0fe7e72eb4fa
allly <allysmith64000@gmail.com>
2024-12-22 15:47:01 -0700

    programed chasie movement for yellow blocks in auto for both sides

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -9,27 +9,63 @@ import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
-
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(23, 70, 0))
+
+                        //place block 1
                         .lineToLinearHeading(new Pose2d(40, 40, Math.toRadians(-90)))
-                        .strafeLeft(8)
-                        .waitSeconds(2)
-                        .strafeLeft(12)
                         .lineToLinearHeading(new Pose2d(55, 55, Math.toRadians(-135)))
-                        .waitSeconds(8)
+                        .waitSeconds(6)
+
+                        //pick up block 2
                         .lineToLinearHeading(new Pose2d(40, 40, Math.toRadians(270)))
                         .strafeLeft(18.50)
                         .waitSeconds(2)
+
+                        //place block 2
                         .strafeLeft(-19)
                         .lineToLinearHeading(new Pose2d(55, 55, Math.toRadians(-135)))
-                        .waitSeconds(8)
+                        .waitSeconds(6)
+
+                        //park at submersible, level 1 hang
                         .lineToLinearHeading(new Pose2d(40, 40, Math.toRadians(-90)))
                         .forward(30)
-                        .strafeRight(17)
+                        .turn(Math.toRadians(-90))
+                        .forward(16)
                         .waitSeconds(4)
+
+                        .build());
+
+        RoadRunnerBotEntity MyBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-23, -70, 0))
+
+                        //place block 1
+                        .lineToLinearHeading(new Pose2d(-40, -40, Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(-52, -52, Math.toRadians(45)))
+                        .waitSeconds(6)
+
+                        //pick up block 2
+                        .lineToLinearHeading(new Pose2d(-40, -40, Math.toRadians(90)))
+                        .forward(4)
+                        .strafeLeft(19)
+                        .waitSeconds(2)
+
+                        //place block 2
+                        .strafeLeft(-19)
+                        .lineToLinearHeading(new Pose2d(-52, -52, Math.toRadians(45)))
+                        .waitSeconds(6)
+
+                        //park at submersible, level 1 hang
+                        .lineToLinearHeading(new Pose2d(-40, -40, Math.toRadians(90)))
+                        .forward(30)
+                        .turn(Math.toRadians(-90))
+                        .forward(16)
+                        .waitSeconds(4)
+
 
                         .build());
 
@@ -38,6 +74,7 @@ public class MeepMeepTesting {
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
+                .addEntity(MyBot)
                 .start();
     }
 }

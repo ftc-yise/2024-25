70bb636bbf4ce0e9466e342fccf4cf69a57d67d3
allly <allysmith64000@gmail.com>
2024-12-28 15:44:44 -0700

    added auto programs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoBluePark.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.yise.LiftClass;
+import org.firstinspires.ftc.teamcode.yise.ledLights;
+
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.yise.LiftClass;
+import org.firstinspires.ftc.teamcode.yise.ledLights;
+// import org.firstinspires.ftc.teamcode.yise.Parameters;
+// import org.firstinspires.ftc.teamcode.yise.poseStorage;
+
+@Autonomous(name = "yellowAuto", group = "Linear Opmode")
+
+public class autoBluePark extends LinearOpMode {
+    public float endLocation_X = 0;
+    public float endLocation_Y = 16;
+    public float endHeading_Z = 90;
+
+    @Override
+    public void runOpMode() {
+
+        // ------------------------------------------------------------------------------------
+        // Initialize Class Instances and Variables
+        // ------------------------------------------------------------------------------------
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        LiftClass arm = new LiftClass(hardwareMap);
+        ledLights leds = new ledLights(hardwareMap);
+        // poseStorage.currentPose = drive.getPoseEstimate();
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        leds.setLed(ledLights.ledStates.INIT);
+
+        // ------------------------------------------------------------------------------------
+        // Define Trajectories and Arm/Grabber Actions
+        // ------------------------------------------------------------------------------------
+
+        // Start by defining our start position
+        Pose2d startPose = new Pose2d(23, 70, 0);
+        drive.setPoseEstimate(startPose);
+
+        TrajectorySequence blue_Side_Park = drive.trajectorySequenceBuilder(startPose)
+                .strafeRight(-30)
+                .turn(Math.toRadians(-90))
+                .strafeLeft(-17)
+                .forward(-30)
+                .turn(Math.toRadians(90))
+                .forward(16)
+                .waitSeconds(2)
+
+                .build();
+
+        // run my trajectories in order
+
+        // telemetry.addData("Distance S Left", yiseDrive.distanceSensorLeft);
+        // telemetry.addData("Distance S Right", yiseDrive.distanceSensorRight);
+        telemetry.update();
+
+        // drive to cone stack with arm at cone 5 height
+        drive.followTrajectorySequence(blue_Side_Park);
+        // telemetry.addData("Distance S Left", yiseDrive.distanceSensorLeft);
+        // telemetry.addData ("Distance S Right", yiseDrive.distanceSensorRight);
+        telemetry.update();
+
+        //drive.followTrajectorySequence(seq_2);
+
+
+    }}

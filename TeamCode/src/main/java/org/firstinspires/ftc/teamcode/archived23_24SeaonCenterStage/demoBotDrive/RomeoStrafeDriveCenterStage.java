9a174c6845d74c89ba8e02cb55d6076ed1cd441b
jacob <jacobwingate@outlook.com>
2025-10-05 13:03:07 -0600

    ball bot augmenting it to so we can use roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/archived23_24SeaonCenterStage/demoBotDrive/RomeoStrafeDriveCenterStage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/archived23_24SeaonCenterStage/demoBotDrive/RomeoStrafeDriveCenterStage.java
@@ -60,8 +60,8 @@ public class RomeoStrafeDriveCenterStage extends LinearOpMode {
 
             // Combine the joystick requests for each axis-motion to determine each wheel's power.
             // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = forward + strafe + turn;
-            double rightFrontPower = forward - strafe - turn;
+            double leftFrontPower  = -forward + strafe + turn;
+            double rightFrontPower = -forward - strafe - turn;
             double leftBackPower   = forward - strafe + turn;
             double rightBackPower  = forward + strafe - turn;
 
